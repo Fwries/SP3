f3e0adec6c4b4f3badd6286b5fcace2deee485d2
JH-Chong <88106151+JH-Chong@users.noreply.github.com>
2022-08-17 14:06:23 +0800

    Player Update

--- App/Source/Scene2D/Player2D.cpp
+++ App/Source/Scene2D/Player2D.cpp
@@ -471,7 +471,8 @@ void CPlayer2D::Update(const double dElapsedTime)
 	{
 		vec2Index = glm::vec2((float)cSettings->NUM_TILES_XAXIS / 2.f, (float)cSettings->NUM_TILES_YAXIS / 2.f);
 		// Make sure that the lives are 0 before adding 100 to lives
-		cInventoryManager->GetItem("Lives")->Add((cInventoryManager->GetItem("Lives")->GetCount() * 0) + 100);
+		cInventoryManager->GetItem("Lives")->Remove(cInventoryManager->GetItem("Lives")->GetCount());
+		cInventoryManager->GetItem("Lives")->Add(100);
 	}
 
 	// Randomly generates tiles
