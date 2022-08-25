dab50ab874f2318092528c9620989136e254d96a
Fwries <totallynotforfaries@gmail.com>
2022-08-25 15:00:38 +0800

    Fixed Stuff

--- App/Source/Scene2D/Turret.cpp
+++ App/Source/Scene2D/Turret.cpp
@@ -39,13 +39,13 @@ CTurret::CTurret(void)
 	transform = glm::mat4(1.0f);	// make sure to initialize matrix to identity matrix first
 
 	// Initialise vecIndex
-	vec2Index = glm::i32vec2(0);
+	//vec2Index = glm::i32vec2(0);
 
 	// Initialise vecNumMicroSteps
 	i32vec2NumMicroSteps = glm::i32vec2(0);
 
 	// Initialise vec2UVCoordinate
-	vec2UVCoordinate = glm::vec2(0.0f);
+	//vec2UVCoordinate = glm::vec2(0.0f);
 
 	i32vec2Destination = glm::i32vec2(0, 0);	// Initialise the iDestination
 	i32vec2Direction = glm::i32vec2(0, 0);		// Initialise the iDirection
